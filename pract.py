7aac21fe8c045088f3db46bae71e426f651e31ce
yoonkeen <agy91521947@gmail.com>
2024-12-14 22:32:53 +0900

    끝!

---
+++ pract.py
@@ -0,0 +1,22 @@
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dic = {"1":1, "2":2}
+dic[3] = 5
+a = dic.keys()
+
+
+
+li = ["abs","sdf","fa"]
+arr1 = np.arange(1,10, 2)
+arr2 = np.random.randint(1,3, 1,2)
+
+
+print(arr1, arr2)
+
+
+
+
+
+
